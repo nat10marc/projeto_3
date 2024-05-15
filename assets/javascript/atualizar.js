f18ff6dc797481ebde66428bdb81e43f60f14f58
nat10marc <natanmarc95777@gmail.com>
2024-05-15 08:00:20 -0300

    apagar e atualizar

---
+++ assets/javascript/atualizar.js
@@ -0,0 +1,22 @@
+function inc(id){
+
+    const tarefas = JSON.parse( localStorage.getItem("tarefas") ) || []
+    let tarefa = tarefas.find( tarefa => tarefa.id = id)
+    tarefa.status += 10
+    localStorage.setItem("tarefas", JSON.stringify( tarefas) )
+
+    document.querySelector("#" + id + " progress").value += 10
+
+}
+
+function dec(id){
+
+    const tarefas = JSON.parse( localStorage.getItem("tarefas") ) || []
+    let tarefa = tarefas.find( tarefa => tarefa.id = id)
+    tarefa.status -= 10
+    if (tarefa.status < 0) tarefa.status = 0
+    localStorage.setItem("tarefas", JSON.stringify( tarefas) )
+
+    document.querySelector("#" + id + " progress").value = tarefa.status
+
+}
--- assets/javascript/carregar.js
+++ assets/javascript/carregar.js
@@ -1,28 +1,24 @@
-const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+const tarefas = JSON.parse( localStorage.getItem("tarefas") ) || []
 
 tarefas.forEach(tarefa => card(tarefa))
 
-function card(tarefa){
+function card (tarefa){
     const cardTarefa = `
-    
-    <div class="nes-container with-title is-centered">
-    <p class="title">Figma</p>
-    <p>Criar o protótipo do app</p>
-    <a href="#" class="nes-badge is-icon">
-        <span class="is-warning"><i class="nes-icon star is-small"></i></span>
-        <span class="is-primary">150</span>
-    </a>
-    <progress class="nes-progress is-success" value="50" max="100"></progress>
-    <button type="button" class="nes-btn is-primary">-</button>
-    <button type="button" class="nes-btn is-error">apagar</button>
-    <button type="button" class="nes-btn is-primary">+</button>
-    
-    
-    </div>
-    
-    `
-
+        <div class="nes-container with-title is-centered">
+            <p class="title">${tarefa.titulo}</p>
+            <p>${tarefa.descricao}</p>
+            <a href="#" class="nes-badge is-icon">
+                <span class="is-warning"><i class="nes-icon coin is-small"></i></span>
+                <span class="is-primary">${tarefa.pontos}</span>
+            </a>
+            <progress class="nes-progress is-success" value="0" max="100"></progress>
+            <button onClick="dec('${tarefa.id}')" type="button" class="nes-btn is-primary">-</button>
+            <button onClick="apagar('${tarefa.id}')" type="button" class="nes-btn is-error">apagar</button>
+            <button onClick="inc('${tarefa.id}')" type="button" class="nes-btn is-primary">+</button>
+        </div>    
+    ` 
     const card = document.createElement("div")
+    card.id = tarefa.id
     card.innerHTML = cardTarefa
     document.querySelector("#lista-de-tarefas").appendChild(card)
 }
